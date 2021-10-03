400126f7080b12719face00e22b9886e5d5d4d19
Fish Chen <lovefish0416@gmail.com>
2021-10-03 14:18:49 +0800

    Create a template

--- blog/settings.py
+++ blog/settings.py
@@ -56,7 +56,8 @@ ROOT_URLCONF = 'blog.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        # 加入templates目錄
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
