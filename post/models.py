f7344688c6b38cb75bc2595f9b9ca5df8e91ac37
Fish Chen <lovefish0416@gmail.com>
2021-10-03 14:03:52 +0800

    register 'post' form to admin

--- post/models.py
+++ post/models.py
@@ -2,5 +2,10 @@ from django.db import models
 
 # Create your models here.
 class Post(models.Model):
-    title = models.CharField('標題', max_length=20)
+    def __str__(self):
+        return self.title
+    class Meta:
+        verbose_name = '文章'
+        verbose_name_plural = '文章'
+    title = models.CharField('標題', max_length=20, )
     content = models.CharField('內容', max_length=200)
